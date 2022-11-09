3d09c2e71ead052cdff8fe03a47338aed3400116
YasinCap <yasin.gumus@capgemini.com>
2022-11-09 10:13:41 +0100

    finale

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../components/components/src/stories/Button.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,38 @@
 import { createClient } from "contentful";
+import { Button } from '../components/components/src/stories/Button';
 
 export async function getStaticProps() {
 
-const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-})
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
 
-const res = await client.getEntries({content_type: "post"})
+  const res = await client.getEntries({ content_type: "post" })
 
-return {
-  props: {
-    posts: res.items
+  return {
+    props: {
+      posts: res.items
+    }
   }
 }
-}
-  
-export default function Blogposts({posts}) {
+
+export default function Blogposts({ posts }) {
   console.log(posts)
 
   return (
     <div className="blogposts">
       {posts.map(posts => (
-        <div key={posts.sys.id}> 
-        
-        <h1>{posts.fields.title}</h1>
-        <img src={posts.fields.coverImage.fields.file.url}></img>
-        </div> 
+        <div key={posts.sys.id}>
+
+          <h1>{posts.fields.title}</h1>
+          <img src={posts.fields.coverImage.fields.file.url}></img>
+          <Button
+            label= {posts.fields.title}
+            onClick={() => { }}
+            primary
+          />
+        </div>
       ))}
     </div>
   )

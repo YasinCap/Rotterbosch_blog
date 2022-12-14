97541bdba2b40371ce9539ce37147f0bd950e34f
YasinCap <yasin.gumus@capgemini.com>
2022-12-14 10:55:46 +0100

    blogs tekst op main page

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Blogposts({ posts }) {
         <div className="relative mx-auto max-w-7xl">
           <div className="text-center">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Gemeente Rotterbosch
+              Blogs
             </h2>
             <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-500 sm:mt-4"></p>
           </div>

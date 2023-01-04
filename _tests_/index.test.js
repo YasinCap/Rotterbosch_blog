d3a6cdb6130b85ebdfea6d9f7ce3faf1c283f91b
YasinCap <yasin.gumus@capgemini.com>
2023-01-04 13:52:13 +0100

    added comments to the tests

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -8,16 +8,22 @@ import Blogposts from "../pages";
 import { mockPosts } from "./index.mock.js";
 
 describe("Navbar", () => {
+  // this test checks if the text Gemeente Rotterbosch gets rendered within the navbar component
   it("renders the navbar with correct title", () => {
+    // here I set the const to render the component
     const { getByText } = render(<Navbar />);
+    // here I define which text it should look for
     const element = getByText("Gemeente Rotterbosch");
     expect(element).toBeInTheDocument();
   });
 });
 
 describe("Index", () => {
+  // this test checks if the title of a specific blog gets rendered
   it("renders the blog overview with a correct title", () => {
+    // the test uses the render function to render the blogpost within the blogpost function
     const { getByText } = render(<Blogposts posts={mockPosts} />);
+    // here I define which text it should look for
     const element = getByText(
       "Nieuwe kenniswerkplaats Onbegrepen Gedrag wil het onbegrijpelijke omarmen"
     );

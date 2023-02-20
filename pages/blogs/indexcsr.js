c3f4bc548faeeb8c668081d73c608c8952982c43
YasinCap <yasin.gumus@capgemini.com>
2023-02-20 10:53:21 +0100

    csr

--- pages/blogs/indexcsr.js
+++ pages/blogs/indexcsr.js
@@ -31,7 +31,7 @@ export default function Blogposts() {
         <div className="relative mx-auto max-w-7xl " title="cards">
           <div className="text-center">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Client Side Rendering
+              Server Side Rendering
             </h2>
             <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-500 sm:mt-4"></p>
           </div>

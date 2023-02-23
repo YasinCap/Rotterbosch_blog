ac7bd0b39dda23c5ebd461f747cf3ae9f77a3c8a
YasinCap <yasin.gumus@capgemini.com>
2023-02-23 14:45:47 +0100

    Veel data

--- pages/blogs/indexcsr.js
+++ pages/blogs/indexcsr.js
@@ -60,7 +60,141 @@ export default function Blogposts() {
                     <p className="text-xl font-semibold text-gray-900">
                       {post.fields.title}
                     </p>
-
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[0].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[1].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[2].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[0].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[1].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[2].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[0].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[1].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[2].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[0].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[1].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[2].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[0].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[1].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
+                    <video
+                      autoPlay
+                      muted
+                      style={{ width: "500px", height: "500px" }}
+                    >
+                      <source
+                        src={posts[2].fields.videoOfFoto.fields.file.url}
+                      />
+                    </video>
                     <p className="mt-3 text-base text-gray-500">
                       {post.fields.cliffhanger}
                     </p>
